14ba4b480307f3a49229c494ea6a13d8aef05d71
Vamsi <vamsi.gpv@gmail.com>
2016-11-30 23:09:14 -0500

    commited message

---
+++ localPoc/src/test/java/com/att/salesexpress/poc/SalesExpressPocApplicationTests.java
@@ -0,0 +1,17 @@
+package com.att.salesexpress.poc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SalesExpressPocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		System.out.println("commited");
+	}
+
+}
