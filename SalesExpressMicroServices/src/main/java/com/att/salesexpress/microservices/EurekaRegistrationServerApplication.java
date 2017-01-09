99cbea90579d5ee7302d2eab05a66a09f7ff79a3
sachin w <swadhank@us.ibm.com>
2017-01-09 15:20:13 -0500

    Changed to create a war file instead of jar

--- SalesExpressMicroServices/src/main/java/com/att/salesexpress/microservices/EurekaRegistrationServerApplication.java
+++ SalesExpressMicroServices/src/main/java/com/att/salesexpress/microservices/EurekaRegistrationServerApplication.java
@@ -4,16 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableEurekaServer
+//@ComponentScan(basePackages = { "com.att.salesexpress.microservices" })
 @EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
-public class EurekaRegistrationServerApplication {
+public class EurekaRegistrationServerApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EurekaRegistrationServerApplication.class);
+	}
 
 	public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(EurekaRegistrationServerApplication.class);
 		System.setProperty("spring.config.name", "eureka-registration-server");
-		SpringApplication.run(EurekaRegistrationServerApplication.class, args);
+        application.run(args);
 	}
 }
--- SalesExpressMicroServices/src/main/java/com/att/salesexpress/microservices/SalesExpressMicroServicesApplication.java
+++ SalesExpressMicroServices/src/main/java/com/att/salesexpress/microservices/SalesExpressMicroServicesApplication.java
@@ -2,14 +2,22 @@ package com.att.salesexpress.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class SalesExpressMicroServicesApplication {
+public class SalesExpressMicroServicesApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EurekaRegistrationServerApplication.class);
+	}
+	
 	public static void main(String[] args) {
-		System.setProperty("spring.config.name", "salesexpress-microservices");
-		SpringApplication.run(SalesExpressMicroServicesApplication.class, args);
+        SpringApplication application = new SpringApplication(SalesExpressMicroServicesApplication.class);
+        System.setProperty("spring.config.name", "salesexpress-microservices");
+        application.run(args);
 	}
 }
