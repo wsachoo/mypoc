3207020ba2fb79cc663dfa12d2be6b0b23993fc3
vamsigk <vg8736@att.com>
2017-04-18 14:19:51 -0400

    included successratio, added customer legal info block, increased the size of the modal

--- salesapp-microservices/salesapp/src/main/resources/static/js/solutionTemplate/step_wizard_config.js
+++ salesapp-microservices/salesapp/src/main/resources/static/js/solutionTemplate/step_wizard_config.js
@@ -1,44 +1,44 @@
 var userSolTmplSelectionObject = {}; //global object for user selection on step wizard
 
 function showSalesHistoryGrid(userSolTmplSelectionObject) {
-	
+
 	//console.log("userSolTmplSelectionObject : from showSalesHistoryGrid" + JSON.stringify(userSolTmplSelectionObject));
 	var jsonData = JSON.stringify(userSolTmplSelectionObject);
- 	var promise = httpAsyncPostWithJsonRequestResponse("http://localhost:8080/user/salesHistory/getRecommendationBasedOnSalesHistory", jsonData);
+	var promise = httpAsyncPostWithJsonRequestResponse("http://localhost:8080/user/salesHistory/getRecommendationBasedOnSalesHistory", jsonData);
 	promise.done(function(data, textStatus, jqXHR){
-			//console.log("Data :" + JSON.stringify(data));
-			$("#solutionTemplateBottomFrame").empty();
-			if(data["STATUS"] != null && data["STATUS"] != "" && data["STATUS"] == "SUCCESS" && data["DATA"].length>0){
-				var gridTable = '<table id="grid-data" class="table table-condensed table-hover" style="background-color : #F5F5F5;">'+
-					    			'<thead>'+
-							    		 '<tr>'+
-							    		 	 '<th data-column-id="commands" data-formatter="commands" data-sortable="false">View</th>'+
-								    		 '<th data-column-id="accessSpeed" >ACCESS SPEED</th>'+
-								    		 '<th data-column-id="ethernetHandoffInterface">ETHERNET HANDOFF INTERFACE</th>'+
-								    		 '<th data-column-id="designType">DESIGN TYPE</th>'+
-								    		 '<th data-column-id="portSpeed">PORT SPEED</th>'+
-								    		 '<th data-column-id="managedRouter">MANAGED ROUTER</th>'+
-								    		 '<th data-column-id="siteNameAlias">SITE NAME ALIAS</th>'+
-								    		 '<th data-column-id="ratePlan">RATE PLAN</th>'+
-								    		 '<th data-column-id="tailTechnologyId">TAIL TECHNOLOGY</th>'+
-							    		 '</tr>'+
-					    			'</thead>'+
-					    		'</table>';
+		console.log("Data :" + JSON.stringify(data));
+		$("#solutionTemplateBottomFrame").empty();
+		if(data["STATUS"] != null && data["STATUS"] != "" && data["STATUS"] == "SUCCESS" && data["DATA"].length>0){
+			var gridTable = '<table id="grid-data" class="table table-condensed table-hover" style="background-color : #F5F5F5;">'+
+			'<thead>'+
+			'<tr>'+
+			'<th data-column-id="commands" data-formatter="commands" data-sortable="false">View</th>'+
+			'<th data-column-id="accessSpeed" >ACCESS SPEED</th>'+
+			'<th data-column-id="ethernetHandoffInterface">ETHERNET HANDOFF INTERFACE</th>'+
+			'<th data-column-id="designType">DESIGN TYPE</th>'+
+			'<th data-column-id="portSpeed">PORT SPEED</th>'+
+			'<th data-column-id="managedRouter">MANAGED ROUTER</th>'+
+			'<th data-column-id="siteNameAlias">SITE NAME ALIAS</th>'+
+			'<th data-column-id="ratePlan">RATE PLAN</th>'+
+			/* '<th data-column-id="tailTechnologyId">TAIL TECHNOLOGY</th>'+*/
+			'</tr>'+
+			'</thead>'+
+			'</table>';
 			$("#solutionTemplateBottomFrame").append(gridTable);
 			var data = data["DATA"];
-			$.each(data, function(k,v) { 
+			$.each(data, function(k,v) {
 				var gridRow = 	'<tr >'+
-								 	'<td data-column-id="commands"></td>'+
-    								'<td data-column-id="accessSpeed">'+ data[k].accessSpeed +'</td>'+
-    								'<td data-column-id="ethernetHandoffInterface">'+ data[k].ethernetHandoffInterface +'</td>'+
-    								'<td data-column-id="designType" data-order="desc">'+ data[k].designType +'</td>'+
-    								'<td data-column-id="portSpeed">'+ data[k].portSpeed +'</td>'+
-    								'<td data-column-id="managedRouter">'+ data[k].managedRouter +'</td>'+
-    								'<td data-column-id="siteNameAlias">'+ data[k].siteNameAlias +'</td>'+
-    								'<td data-column-id="ratePlan">'+ data[k].ratePlan +'</td>'+
-    								'<td data-column-id="tailTechnologyId">'+ data[k].tailTechnologyId +'</td>'+
-    							'</tr>'
-    			$("#grid-data").append(gridRow);			
+				'<td data-column-id="commands"></td>'+
+				'<td data-column-id="accessSpeed">'+ data[k].accessSpeed +'</td>'+
+				'<td data-column-id="ethernetHandoffInterface">'+ data[k].ethernetHandoffInterface +'</td>'+
+				'<td data-column-id="designType" data-order="desc">'+ data[k].designType +'</td>'+
+				'<td data-column-id="portSpeed">'+ data[k].portSpeed +'</td>'+
+				'<td data-column-id="managedRouter">'+ data[k].managedRouter +'</td>'+
+				'<td data-column-id="siteNameAlias">'+ data[k].siteNameAlias +'</td>'+
+				'<td data-column-id="ratePlan">'+ data[k].ratePlan +'</td>'+
+				/*'<td data-column-id="tailTechnologyId">'+ data[k].tailTechnologyId +'</td>'+*/
+				'</tr>'
+				$("#grid-data").append(gridRow);			
 			});
 			var grid = $("#grid-data");
 			$("#grid-data").bootgrid({
@@ -58,16 +58,17 @@ function showSalesHistoryGrid(userSolTmplSelectionObject) {
 					var thisElementId = $(this);
 					var rowId = thisElementId.closest('tr').attr('data-row-id');
 					var url = data[rowId].links[0].href;
-					displaySelectedRowModal(url);
+					var matchpercentage = data[rowId].matchPercentage;
+					displaySelectedRowModal(url, matchpercentage);
 				});
 
 			});
-			}
-			else{
-				$("#solutionTemplateBottomFrame").empty();
-				$("#solutionTemplateBottomFrame").html(data["ERROR_DESC"])
-			}
-			
+		}
+		else{
+			$("#solutionTemplateBottomFrame").empty();
+			$("#solutionTemplateBottomFrame").html(data["ERROR_DESC"])
+		}
+
 	}).fail(function(jqXHR, textStatus, errorThrown) {
 		console.log(textStatus);
 	}); 
@@ -91,6 +92,11 @@ function highlightActiveStep(stepNumber) {
 function initializeStepWizard() {
 	var promise = httpAsyncPostWithJsonRequestResponse(contextPath + "/user/solutionTemplate/getAllQuestions", "{}");
 	promise.done(function(data, textStatus, jqXHR ) {
+		data[0].mydesc = "Access Type";
+		data[1].mydesc = "Access Speed";
+		data[2].mydesc = "Port Speed";
+		data[3].mydesc = "Rate Type";
+		console.log("YYY:" + JSON.stringify(data));
 		var stepWizardData = $("#stepwizard-step-template").tmpl(data);
 		stepWizardData.appendTo("#stepWizardId");
 		populateWizardElement(data[0].quesSeqId);
@@ -142,13 +148,13 @@ function generateUserSelectObject() {
 		var radioElValue = $(this).val();
 		userSolTmplSelectionObject[radioElName] = radioElValue;
 	});
-	
+
 	$("#formId select").each(function() {
 		var selectElName = $(this).attr('name');
 		var selectElValue = $("select[name='"+ selectElName +"'] option:selected").val();
 		userSolTmplSelectionObject[selectElName] = selectElValue;
 	});
-	
+
 	var checkElNames = []
 	$('#formId input[type="checkbox"]:checked').each(function() {
 		checkElNames.push($(this).attr('name'));
@@ -164,41 +170,42 @@ function generateUserSelectObject() {
 	});
 }
 function onStepContentFormClick(e) {
-	  var eventSourceId = e.target.id;
-	  if (eventSourceId.indexOf("btnNext-") >= 0) {
-			var quesSeqId = eventSourceId.substring(eventSourceId.indexOf("-") + 1);
-			quesSeqId++;
-			populateWizardElement(quesSeqId);
-	  }
+	var eventSourceId = e.target.id;
+	if (eventSourceId.indexOf("btnNext-") >= 0) {
+		var quesSeqId = eventSourceId.substring(eventSourceId.indexOf("-") + 1);
+		quesSeqId++;
+		populateWizardElement(quesSeqId);
+	}
 
-	  if (eventSourceId.indexOf("btnPrev-") >= 0) {
-			var quesSeqId = eventSourceId.substring(eventSourceId.indexOf("-") + 1);
-			quesSeqId--;
-			populateWizardElement(quesSeqId);
-	  }  
+	if (eventSourceId.indexOf("btnPrev-") >= 0) {
+		var quesSeqId = eventSourceId.substring(eventSourceId.indexOf("-") + 1);
+		quesSeqId--;
+		populateWizardElement(quesSeqId);
+	}  
 }
 
 function onStepWizardClick(e) {
-	 var eventSourceName = e.target.name;
-	  
-	  if ("btnSolutionTemplateSteps" == eventSourceName) {
+	var eventSourceName = e.target.name;
+
+	if ("btnSolutionTemplateSteps" == eventSourceName) {
 		e.preventDefault();	
 		var stepNumber = e.target.text; //stepNumber is designed to be same ad question sequence number
 		populateWizardElement(stepNumber);
-	  }
+	}
 }
 
-function displaySelectedRowModal(url) {
+function displaySelectedRowModal(url, matchPercentage) {
 	
 	var data = httpGetWithJsonResponse(url, "");
 	$("body").remove("#stepwizard-display-selected-row-modal");
 	var DATA = {};
 	var objectKeysArray = ["accessType", "accessSpeed", "portType", "portSpeed", "designName", "accessService", "ipVersionLabel", "protocol", "routingProtocol", "tailTechnology", "bundleCd","ratePlan"];
 
-	$.each(objectKeysArray, function(key, value) {
+	$.each(objectKeysArray, function(k, value) {
 		var key = value.replace(/([a-z])([A-Z])/g, '$1 $2').toUpperCase();
 		DATA[key] = data[value];
 	});
+	DATA["SUCCESS RATIO"] = matchPercentage;
 	var modalDataContent = $("#stepwizard-display-selected-row-modal").tmpl(DATA);
 	modalDataContent.appendTo('body');
 	$("#btnDisplayRowSelectModal").trigger('click');
