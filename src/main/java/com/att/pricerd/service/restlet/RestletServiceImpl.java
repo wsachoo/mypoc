b25de5a0d1518fd7511b70771a9eb1bb86c3db24
sachin w <swadhank@us.ibm.com>
2017-06-16 15:20:13 -0400

    Modified the sample message

--- src/main/java/com/att/pricerd/service/restlet/RestletServiceImpl.java
+++ src/main/java/com/att/pricerd/service/restlet/RestletServiceImpl.java
@@ -1,16 +1,20 @@
 package com.att.pricerd.service.restlet;
 
 import org.apache.camel.Exchange;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RestletServiceImpl implements RestletService {
+
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+
 	public RestletServiceImpl() {
 	}
 
 	public final void speak(Exchange e) {
 		e.setOut(e.getIn());
-		e.getOut().setBody("Hello World From SalesAppService!");
+		e.getOut().setBody("Hello World From SalesAppService!. Going to connect to Oracle DB at : " + dbUrl);
 	}
 }
